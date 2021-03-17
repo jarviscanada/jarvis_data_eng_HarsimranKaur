6d53046f8832b047a14dd8d837669379eeacd660
Harsimran Kaur <simmiglani@gmail.com>
2021-03-17 15:15:50 +0000

    Merged monitoring_agent into develop

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,26 @@
+psql_host=$1;
+psql_port=$2;
+db_name=$3;
+psql_user=$4;
+psql_password=$5;
+
+    "timestamp" TIMESTAMP NOT NULL,
+    host_id SERIAL NOT NULL,
+    memory_free INTEGER NOT NULL,
+    cpu_idle INTEGER NOT NULL,
+    cpu_kernel INTEGER NOT NULL,
+    disk_io INTEGER NOT NULL,
+    disk_available
+
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+memory_free=$(cat /proc/meminfo | egrep "^MemFree\:" | awk '{print $2}' | xargs)
+cpu_idle=$(vmstat -t|tail -1|awk '{print $15}')
+cpu_kernel=$(vmstat -t| tail -1|awk '{print $14}')
+disk_io=$(vmstat --unit M | tail -1 | awk '{print $9}')
+disk_available=$(df -BM | awk 'NR==2{print substr($4, 1, length($4)-1)}')
+
+insert_stmt="INSERT INTO host_usage(timestamp,memory_free,cpu_idle,cpu_kernel,disk_io,disk_available)
+VALUES('$timestamp', $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available);"
+
+export PGPASSWORD=$psql_password
+psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$insert_stmt"
